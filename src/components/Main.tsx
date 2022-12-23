da04cdc4ddfba26c6b94f79ba3f1e785b50198a3
Elias Vedovato <eliasvedovato@gmail.com>
2022-12-23 15:40:34 -0300

    te deje un api para probar

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,33 +1,31 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 
 type resultProps = {
-    name: string;
-    price: number;
-    description: string;
-    available: boolean;
+	name: string
+	price: number
+	description: string
+	available: boolean
 }
 
 const main = () => {
+	const [result, setResult] = useState<resultProps[]>([])
 
-const [result, setResult] = useState<resultProps[]>([])
+	useEffect(() => {
+		const api = async () => {
+			const data = await fetch('', {
+				method: 'GET',
+			})
+			const json = await data.json()
+			setResult(json.result)
+		}
 
-    useEffect(() => {
-      
-        const api= async () => {
-            const data = await fetch("", {
-                method: "GET"
-            });
-            const json = await data.json();
-            setResult(json.result)
-        }
-    
-        api()
-    }, [])
-    
+		api()
+	}, [])
 
-  return (
-    <div>main</div>
-  )
+	// https://api.unsplash.com/photos?query=t-shirt&client_id=vVXwwNev7aLRGfzmwLGuI0sEWbPkv--WXVXPEq9jy4s
+	// proba con esta api a ver que onda
+
+	return <div>main</div>
 }
 
 export default main
