ac5daf8f5df950def428a8c6601225c80a46aeef
Elias Vedovato <eliasvedovato@gmail.com>
2022-12-26 22:15:17 -0300

    si te digo te miento

--- src/components/FetchComponent/Main.tsx
+++ src/components/FetchComponent/Main.tsx
@@ -2,42 +2,37 @@ import React, { useState, useEffect } from 'react'
 import MapComponent from './MapComponent/MapComponent'
 import { CallFetch } from '../../interfaces/CallFetch'
 
-
 interface PropsFetch {
 	arrayCall: CallFetch
 }
 
-
-const Main = ()  =>{
-	const [result, setResult] = useState<CallFetch[]>([{
-		id: "",
-		title: ""
-	}])
+const Main = () => {
+	const [result, setResult] = useState<CallFetch[]>([])
 
 	useEffect(() => {
-		    fetch('https://jsonplaceholder.typicode.com/todos', {
-				method: 'GET',
-			})
-            .then (response => response.json())
-            .then (res => setResult(res))
-		
-	}, [])	
-    
-	console.log(result);
-	
+		fetch('https://jsonplaceholder.typicode.com/todos', {
+			method: 'GET',
+		})
+			.then(response => response.json())
+			.then(res => setResult(res))
+	}, [])
 
-	return <>
-	<h1>hola soy fetch</h1>
-	{/* {result.map (pro => {
-		return(
-			<div key={pro.id}>
-				<p>{pro.id}</p>
-				<h2>{pro.title}</h2>
-			</div>
-		)
-	})} */}
-	
-    </>
+	console.log(result)
+
+	return (
+		<>
+			<h1>hola soy fetch</h1>
+			{result.map(pro => {
+				return (
+					<div style={{ padding: '5px' }} key={pro.id}>
+						<p>{pro.id}</p>
+						<h2>{pro.title}</h2>
+					</div>
+				)
+			})}
+			{/* <MapComponent arrayCall={result} /> */}
+		</>
+	)
 }
 
 export default Main
