e4ea9d40eb17a9307c7a97b48b27d805c24b2d64
Lucas-Quiroga <quiroga.lucasoffice@gmail.com>
2023-01-30 20:55:37 -0300

    arreglando bugs

--- src/context/CartProvider.tsx
+++ src/context/CartProvider.tsx
@@ -12,48 +12,23 @@ interface props {
 
 const CartProvider = ({children}:any) => {
 
-  // const [todos, setTodos] = React.useState<CallFetch[]>([]);
-
   const [itemsCart, setItemsCart] = React.useState<CallFetch[]>([]);
-
-  // const saveTodo = (todo: ITodo) => {
-  //   const newTodo: ITodo = {
-  //     id: Math.random(), // not really unique - but fine for this example
-  //     title: todo.title,
-  //     description: todo.description,
-  //     status: false,
-  //   }
-  //   setTodos([...todos, newTodo])
-  // }
-  
-  // const updateTodo = (id: number) => {
-  //   todos.filter((todo: ITodo) => {
-  //     if (todo.id === id) {
-  //       todo.status = true
-  //       setTodos([...todos])
-  //     }
-  //   })
-  // }
 
   const cleanCart = () => {
     setItemsCart([])
   }
 
   const addToCart = (item:CallFetch) => {
-    const itemIndex = itemsCart.findIndex((item) => item.id === item.id);
-    if (itemIndex !== -1) {
-      setItemsCart(
-        itemsCart.map((p) =>
-          p.id === item.id
-            ? { ...p }
-            : p
-        )
-      );
-    } else {
-      setItemsCart([item, ...itemsCart]);
-    }
-    console.log(itemsCart);
-    
+    const newItem: CallFetch = {
+          id: Math.random(), 
+          userId: Math.random(),
+          price: item.price,
+	        title: item.title,
+	        category: item.category
+        }
+        setItemsCart([...itemsCart, newItem])
+        console.log(itemsCart);
+        
   };
 
   const deleteToCart = (id:number) => {
