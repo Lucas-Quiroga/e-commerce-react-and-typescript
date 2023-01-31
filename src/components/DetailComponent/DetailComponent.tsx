e8999824153eefbbb22bf1ac0aecf4b5a3ee9939
Lucas-Quiroga <quiroga.lucasoffice@gmail.com>
2023-01-31 12:31:57 -0300

    resolver detalle

--- src/components/DetailComponent/DetailComponent.tsx
+++ src/components/DetailComponent/DetailComponent.tsx
@@ -1,46 +1,29 @@
 import React,{useState, useEffect} from 'react'
 import { useParams } from 'react-router-dom';
 import DetailItemView from './DetailItemView/DetailItemView';
+import { CallFetch } from '../../interfaces/CallFetch';
 import api from '../../api';
-
-type CallFetchCategory = "male" | "female";
-
-const INTIAL_STATE:CallFetch[]= [{
-	id: 0,
-	userId: 0,
-	price: 0,
-	title: "",
-	category: "female",
-}]
-
-interface CallFetch{
-	  id: number,
-	  userId: number,
-	  price: number,
-	  title: string,
-	  category: CallFetchCategory
-	}
 
 
 const DetailComponent = () => {
 
-  const [object, setObject] = useState<CallFetch[]>(INTIAL_STATE)
-  const [detailObject, setDetailObject] = useState({})
+  const [object, setObject] = useState<CallFetch | undefined>()
+  const [detailObject, setDetailObject] = useState<CallFetch[]>([])
   const [show, setShow] = useState(false);
 
-  const {detailId} = useParams();
+  const {detailId}= useParams<string>();
 
 
   useEffect(() => {
-	  api.then(res => setObject(res)) 
-    if (detailId) {
-			const filtrado = object.find((elemento) => elemento.id === parseInt(detailId))
+	  api.then((resolve)=>  setObject(resolve.find((elem) => elem.id === parseInt(detailId? detailId : "")))) 
 
-			// setDetailObject(filtrado);
-			setShow(true);
-		} else {
-			setShow(false)
-		}
+
+    // if (detailId) {
+		// 	setDetailObject(object.find((elemento) => elemento.id === parseInt(detailId)));
+		// 	setShow(true);
+		// } else {
+		// 	setShow(false)
+		// }
 	}, [])
 
       // buscar con find el detalle
