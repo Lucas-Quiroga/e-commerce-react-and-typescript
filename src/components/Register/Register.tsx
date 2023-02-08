722b67eac0ef556a83454c2b9b32f04e19796f8d
Elias Vedovato <eliasvedovato@gmail.com>
2023-02-08 19:21:09 -0300

    registrate estilos

--- src/components/Register/Register.tsx
+++ src/components/Register/Register.tsx
@@ -1,70 +1,87 @@
-import React, { useState, useEffect } from "react";
-import Login from "../Login/Login";
+import React, { useState, useEffect } from 'react'
+import Login from '../Login/Login'
 
 const Register = () => {
-  const getRegister = () => {
-    let datos = localStorage.getItem("register");
-    if (datos) {
-      return JSON.parse(datos);
-    } else {
-      return [];
-    }
-  };
+	const getRegister = () => {
+		let datos = localStorage.getItem('register')
+		if (datos) {
+			return JSON.parse(datos)
+		} else {
+			return []
+		}
+	}
 
-  const [register, setRegister] = useState(getRegister());
-  const [nameUser, setNameUser] = useState("");
-  const [passwordUser, setPasswordUser] = useState("");
-  const [cambioDeComponente, setCambioDeComponente] = useState(false);
+	const [register, setRegister] = useState(getRegister())
+	const [nameUser, setNameUser] = useState('')
+	const [passwordUser, setPasswordUser] = useState('')
+	const [cambioDeComponente, setCambioDeComponente] = useState(false)
 
-  const enviarDatos = (e: any) => {
-    e.preventDefault();
+	const enviarDatos = (e: any) => {
+		e.preventDefault()
 
-    let user = {
-      nameUser,
-      passwordUser,
-    };
+		let user = {
+			nameUser,
+			passwordUser,
+		}
 
-    setRegister([...register, user]);
-    setCambioDeComponente(true);
-  };
+		setRegister([...register, user])
+		setCambioDeComponente(true)
+	}
 
-  //cuando haya cambios en los registros los guarda en el localStorage (pero no es permanente)
-  useEffect(() => {
-    localStorage.setItem("register", JSON.stringify(register));
-  }, [register]);
+	//cuando haya cambios en los registros los guarda en el localStorage (pero no es permanente)
+	useEffect(() => {
+		localStorage.setItem('register', JSON.stringify(register))
+	}, [register])
 
-  console.log(register);
+	console.log(register)
 
-  return (
-    <div className="register_container">
-      {cambioDeComponente === false ? (
-        <div>
-          <h1>Por favor registrate</h1>
-          <form onSubmit={enviarDatos}>
-            <label>Enter your username:</label>
-            <input
-              id="text"
-              type="text"
-              name="text"
-              onChange={(e) => setNameUser(e.target.value)}
-              required
-            />
-            <br />
-            <label>Enter your password:</label>
-            <input
-              type="password"
-              name="password"
-              onChange={(e) => setPasswordUser(e.target.value)}
-              required
-            />
-            <button>guardar datos</button>
-          </form>
-        </div>
-      ) : (
-        <Login />
-      )}
-    </div>
-  );
-};
+	return (
+		<div
+			style={{
+				display: 'flex',
+				flexDirection: 'column',
+				alignItems: 'center',
+			}}
+		>
+			{cambioDeComponente === false ? (
+				<div>
+					<h1 style={{textAlign: 'center'}}>Por favor registrate</h1>
+					<form
+						onSubmit={enviarDatos}
+						style={{
+							display: 'flex',
+							flexDirection: 'column',
+							gap: 15,
+						}}
+					>
+						<div>
+							<label>Enter your username: </label>
+							<input
+								id='text'
+								type='text'
+								name='text'
+								onChange={e => setNameUser(e.target.value)}
+								required
+							/>
+						</div>
+						<div>
+							<label>Enter your password: </label>
+							<input
+								type='password'
+								name='password'
+								onChange={e => setPasswordUser(e.target.value)}
+								required
+							/>
+						</div>
 
-export default Register;
+						<button style={{ width: '100%' }}>Guardar datos</button>
+					</form>
+				</div>
+			) : (
+				<Login />
+			)}
+		</div>
+	)
+}
+
+export default Register
