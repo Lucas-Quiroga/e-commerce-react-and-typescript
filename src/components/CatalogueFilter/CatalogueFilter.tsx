f0be7f653cdf544458385fd8d9d75370b81cf67e
Elias Vedovato <eliasvedovato@gmail.com>
2023-02-08 18:31:39 -0300

    gender checkboxes

--- src/components/CatalogueFilter/CatalogueFilter.tsx
+++ src/components/CatalogueFilter/CatalogueFilter.tsx
@@ -1,10 +1,6 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, MouseEvent } from 'react'
 import { useParams } from 'react-router-dom'
 import { NavLink } from 'react-router-dom'
-
-const INTIAL_STATE = {
-	checked: true,
-}
 
 type valueReferences = 'male' | 'female'
 
@@ -17,63 +13,82 @@ interface CallFetch {
 }
 
 const CatalogueFilter = () => {
-	const [male, setMale] = useState(INTIAL_STATE['checked'])
-	const [female, setFemale] = useState(INTIAL_STATE['checked'])
+	const [male, setMale] = useState(false)
+	const [female, setFemale] = useState(false)
 
-	const { categoryId } = useParams()
+	console.log(female, male)
 
 	const handleValue = (data: valueReferences) => {
 		if (data === 'male') {
-			if (male === true) {
-				console.log(data, 'our value')
-			}
 			setMale(!male)
-			console.log({ female })
 		}
 		if (data === 'female') {
-			if (female === true) {
-				console.log(data, 'our value')
-				setMale(!male)
-				console.log(male)
-			}
 			setFemale(!female)
-			console.log({ male })
+		}
+	}
+
+	const handleMaleClick = () => {
+		setMale(!male)
+		if (female === true) {
+			setFemale(!female)
+		}
+	}
+
+	const handleFemaleClick = () => {
+		setFemale(!female)
+		if (male === true) {
+			setMale(!male)
 		}
 	}
 
 	return (
-		<div style={{padding: 15, minWidth: 100}}>
+		<div style={{ padding: 15, minWidth: 100 }}>
 			<form>
 				<h3>Filtros</h3>
 				<input type='checkbox' id='buzos' />
-        <label htmlFor='buzos'>Buzos</label><br/>
-				<input type='checkbox' id='calzas' /> 
-        <label htmlFor='calzas'>Calzas</label><br/>
-				<input type='checkbox' id='camperas' /> 
-        <label htmlFor='camperas'>Camperas</label><br/>
-				<input type='checkbox' id='carteras' /> 
-        <label htmlFor='carteras'>Carteras</label><br/>
+				<label htmlFor='buzos'>Buzos</label>
+				<br />
+				<input type='checkbox' id='calzas' />
+				<label htmlFor='calzas'>Calzas</label>
+				<br />
+				<input type='checkbox' id='camperas' />
+				<label htmlFor='camperas'>Camperas</label>
+				<br />
+				<input type='checkbox' id='carteras' />
+				<label htmlFor='carteras'>Carteras</label>
+				<br />
 			</form>
-			<div className='Genero'>
+			<div>
 				<h3>Gender</h3>
-				<NavLink to='/category/male' style={{textDecoration: 'none', color: 'black'}}>
+				<NavLink
+					to='/category/male'
+					style={{ textDecoration: 'none', color: 'black' }}
+					onClick={handleMaleClick}
+				>
 					<input
 						value='male'
 						type='checkbox'
 						id='male'
 						name='male'
 						onChange={e => handleValue('male')}
+						checked={male}
 					/>{' '}
 					Male
-				</NavLink> <br/>
-				{/* los values tendrian que ir sin las comillas */}
-				<NavLink to='/category/female' style={{textDecoration: 'none', color: 'black'}}>
+				</NavLink>{' '}
+				<br />
+				
+				<NavLink
+					to='/category/female'
+					style={{ textDecoration: 'none', color: 'black' }}
+					onClick={handleFemaleClick}
+				>
 					<input
 						value='Female'
 						type='checkbox'
 						id='female'
 						name='female'
 						onChange={e => handleValue('female')}
+						checked={female}
 					/>{' '}
 					Female
 				</NavLink>
