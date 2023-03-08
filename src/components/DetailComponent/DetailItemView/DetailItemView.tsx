3dd2cafaac4b0403fe2cf65a295952bd881165ba
Lucas-Quiroga <quiroga.lucasoffice@gmail.com>
2023-03-08 19:54:51 -0300

    finish day

--- src/components/DetailComponent/DetailItemView/DetailItemView.tsx
+++ src/components/DetailComponent/DetailItemView/DetailItemView.tsx
@@ -1,38 +1,43 @@
-import React, { useState, useEffect } from 'react'
-import { CallFetch } from '../../../interfaces/CallFetch'
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { CallFetch } from "../../../interfaces/CallFetch";
 
 const DetailItemView = ({ detailObject }: { detailObject: CallFetch }) => {
-	const [loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState(false);
 
-	useEffect(() => {
-		setLoading(true)
-		setTimeout(() => {
-			setLoading(false)
-		}, 1000)
-	}, [])
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+  }, []);
 
-	return (
-		<>
-			{loading ? (
-				<h2 style={{ textAlign: 'center' }}>Loading...</h2>
-			) : (
-				<div
-					style={{
-						display: 'flex',
-            justifyContent: 'center',
-						alignItems: 'center',
-						flexDirection: 'column',
-					}}
-					key={detailObject.id}
-				>
-					<h2>{detailObject.title}</h2>
-					<img src={detailObject.img} height='700'></img>
+  return (
+    <>
+      {loading ? (
+        <h2 style={{ textAlign: "center" }}>Loading...</h2>
+      ) : (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            flexDirection: "column",
+          }}
+          key={detailObject.id}
+        >
+          <h2>{detailObject.title}</h2>
+          <img src={detailObject.img} height="700"></img>
           <p>Price: ${detailObject.price}</p>
           <p>user id: {detailObject.userId}</p>
-				</div>
-			)}
-		</>
-	)
-}
 
-export default DetailItemView
+          <Link to={"/category"}>
+            <button>Volver</button>
+          </Link>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default DetailItemView;
--- src/components/FetchComponent/MapComponent/ItemsComponent/ItemsComponent.tsx
+++ src/components/FetchComponent/MapComponent/ItemsComponent/ItemsComponent.tsx
@@ -19,7 +19,7 @@ interface CallFetch {
 const ItemsComponent = ({ respuesta }: { respuesta: CallFetch }) => {
   const [render, setRender] = useState(false);
 
-  const { addToCart, deleteToCart, cleanCart } = React.useContext(
+  const { addToCart, deleteToCart, cleanCart, itemsCart } = React.useContext(
     CartContext
   ) as TodoContextType;
 
@@ -38,18 +38,18 @@ const ItemsComponent = ({ respuesta }: { respuesta: CallFetch }) => {
       }}
     >
       <h3>{respuesta.title}</h3>
-      <span>ID: {respuesta.id}</span>
+      {/* <span>ID: {respuesta.id}</span>
       <br />
       <span>USER ID: {respuesta.userId}</span>
       <br />
       <span>{respuesta.category}</span>
       <br />
       <span>STOCK: {respuesta.stock}</span>
-      <br />
+      <br /> */}
       <div style={{ display: "flex", justifyContent: "center" }}>
         <img src={respuesta.img} height="300"></img>
       </div>
-      <span>${respuesta.price}</span>
+      <span style={{ fontWeight: 700 }}>${respuesta.price}</span>
       <br />
       <hr style={{ width: "100%" }} />
 
@@ -62,15 +62,33 @@ const ItemsComponent = ({ respuesta }: { respuesta: CallFetch }) => {
             alignItems: "center",
           }}
         >
+          <h6>Artículo agregado al carrito</h6>
           <Link to={"/carrito"}>
             <button>Ir a la compra</button>
           </Link>
-          <Link to={`/detail/${respuesta.id}`}>
+        </div>
+      ) : (
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            width: "100%",
+            gap: 10,
+          }}
+        >
+          <button onClick={() => additem(respuesta)}>Agregar al carrito</button>
+          <Link
+            to={`/detail/${respuesta.id}`}
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              width: "100%",
+              textDecoration: "none",
+            }}
+          >
             <button>Ver detalle</button>
           </Link>
         </div>
-      ) : (
-        <button onClick={() => additem(respuesta)}>Agregar al carrito</button>
       )}
     </div>
   );
